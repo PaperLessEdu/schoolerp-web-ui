111e4b1d5105ccf097c25e55cefc058d12eeacaa
ygawadeedu <37924628+ygawadeedu@users.noreply.github.com>
2018-04-04 18:17:21 +0530

    Add files via upload
    - Changes for db on heroku

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router(__dirname + '/db/db.js');
+var middlewares = jsonServer.defaults();
+var port = Number(process.env.PORT || 3000);
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+  console.log('JSON Server is running')
+});
