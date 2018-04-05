d1ef4b58cfaccdbd13574c1636c988e21bcde4d3
Yuvraj <ygaawde.edu@gmail.com>
2018-04-05 11:05:16 +0530

    Added fix in db.js for students apis

--- db/db.js
+++ db/db.js
@@ -2,6 +2,6 @@ module.exports = function() {
   return {
     employees: require('./employees.json'),
     standards: require('./standards.json'),
-    student-list: require('./student-list.json')
+    students: require('./student-list.json')
   }
 }
