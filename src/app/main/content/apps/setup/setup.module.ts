882254beddf6b39f9873b0e87259ba708a53d982
Yuvraj <ygaawde.edu@gmail.com>
2018-04-05 15:06:21 +0530

    Implemented table view for standards

--- src/app/main/content/apps/setup/setup.module.ts
+++ src/app/main/content/apps/setup/setup.module.ts
@@ -1,55 +1,48 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule, MatChipsModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSortModule, MatTableModule, MatTabsModule, MatStepperModule, MatDividerModule, MatCardModule, MatDatepickerModule, MatSnackBarModule, MatDialogModule } from '@angular/material';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatButtonModule, MatIconModule, MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule, MatDialogModule } from '@angular/material';
 import { AgmCoreModule } from '@agm/core';
 
 import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseWidgetModule } from '@fuse/components/widget/widget.module';
 
 import { StandardsComponent } from './standards/standards.component';
 import { StandardAddComponent } from './standard-add/standard-add.component';
+import { StandardsService } from './standards/standards.service';
 
 const routes = [
   {
     path: 'standards',
-    component: StandardsComponent
+    component: StandardsComponent,
+    resolve  : {
+      data: StandardsService
+  }
   }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
-    CommonModule,
+    
+    CdkTableModule,
 
     MatButtonModule,
-    MatChipsModule,
-    MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatPaginatorModule,
-    MatRippleModule,
-    MatSelectModule,
     MatSortModule,
     MatTableModule,
-    MatTabsModule,
-    MatStepperModule,
-    MatDividerModule,
-    MatCardModule,
-    MatDatepickerModule,
-    MatSnackBarModule,
     MatDialogModule,
 
-    NgxChartsModule,
-    AgmCoreModule.forRoot({
-        apiKey: 'AIzaSyD81ecsCj4yYpcXSLFcYU97PvRsE_X8Bx8'
-    }),
-
-    FuseSharedModule,
-    FuseWidgetModule
+    FuseSharedModule
   ],
-  declarations: [StandardsComponent, StandardAddComponent],
+  declarations: [
+    StandardsComponent, 
+    StandardAddComponent
+  ],
+  providers: [
+    StandardsService
+  ],
   entryComponents: [StandardAddComponent]
 })
 export class SetupModule { }
--- src/app/main/content/apps/setup/standards/standards.component.ts
+++ src/app/main/content/apps/setup/standards/standards.component.ts
@@ -1,9 +1,22 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { fuseAnimations } from '@fuse/animations';
-import { FuseUtils } from '@fuse/utils';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
 import { MatDialog } from '@angular/material';
 
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { fuseAnimations } from '@fuse/animations';
+import { FuseUtils } from '@fuse/utils';
+
 import { StandardAddComponent } from '../standard-add/standard-add.component';
+import { StandardsService } from './standards.service';
 
 @Component({
   selector: 'app-standards',
@@ -14,21 +27,126 @@ import { StandardAddComponent } from '../standard-add/standard-add.component';
 export class StandardsComponent implements OnInit {
 
   standardName: string;
+  
+  dataSource: StandardsDataSource | null;
+  displayedColumns = ['name'];
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild('filter') filter: ElementRef;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog,
+              private standardsService: StandardsService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.dataSource = new StandardsDataSource(this.standardsService, this.paginator, this.sort);
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+              .debounceTime(150)
+              .distinctUntilChanged()
+              .subscribe(() => {
+                  if ( !this.dataSource ) {
+                      return;
+                  }
+                  this.dataSource.filter = this.filter.nativeElement.value;
+              });
+  }
 
   openDialog(): void {
-      let dialogRef = this.dialog.open(StandardAddComponent, {
-        width: '250px',
-        data : { 
-          standardName: this.standardName
-        }
-      });
+    let dialogRef = this.dialog.open(StandardAddComponent, {
+      width: '250px',
+      data : { 
+        standardName: this.standardName
+      }
+    });
 
-      dialogRef.afterClosed().subscribe(result => {
-        this.standardName = result;
-      });
+    dialogRef.afterClosed().subscribe(result => {
+      this.standardName = result;
+      //make post call
+      //refresh list
+
+    });
   }
+}
+
+export class StandardsDataSource extends DataSource<any> {
+  _filterChange = new BehaviorSubject('');
+  _filteredDataChange = new BehaviorSubject('');
+
+  get filteredData(): any {
+      return this._filteredDataChange.value;
+  }
+
+  set filteredData(value: any) {
+      this._filteredDataChange.next(value);
+  }
+
+  get filter(): string {
+      return this._filterChange.value;
+  }
+
+  set filter(filter: string) {
+      this._filterChange.next(filter);
+  }
+
+  constructor(private standardsService: StandardsService,
+              private _paginator: MatPaginator,
+              private _sort: MatSort) {
+    super();
+    this.filteredData = this.standardsService.standards;
+  }
+  
+  connect(): Observable<any> {
+    //return this.standardsService.getStandards();
+    const displayDataChanges = [
+      this.standardsService.onStandardsChanged,
+      this._paginator.page,
+      this._filterChange,
+      this._sort.sortChange
+    ];
+    
+    return Observable.merge(...displayDataChanges).map(() => {
+      let data = this.standardsService.standards.slice();
+      data = this.filterData(data);
+      
+      this.filteredData = [...data];
+
+      data = this.sortData(data);
+
+      // Grab the page's slice of data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return data.splice(startIndex, this._paginator.pageSize);
+    });
+  }
+
+  filterData(data) {
+      if ( !this.filter )
+      {
+          return data;
+      }
+      return FuseUtils.filterArrayByString(data, this.filter);
+  }
+
+  sortData(data): any[] {
+    if ( !this._sort.active || this._sort.direction === '' ) {
+        return data;
+    }
+
+    return data.sort((a, b) => {
+        let propertyA: number | string = '';
+        let propertyB: number | string = '';
+
+        switch ( this._sort.active ) {
+          case 'name':
+            [propertyA, propertyB] = [a.name, b.name];
+            break;
+        }
+
+        const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+        const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+        return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
+    });
+}
+
+  disconnect() {}
 }
