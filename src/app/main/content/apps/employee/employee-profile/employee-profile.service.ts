0964f694d69e72ac68ac5d2f4d98a8549441c8c0
Yuvraj <ygaawde.edu@gmail.com>
2018-05-16 00:18:39 +0530

    Changse in the Empl profile page api base url.

--- src/app/main/content/apps/employee/employee-profile/employee-profile.service.ts
+++ src/app/main/content/apps/employee/employee-profile/employee-profile.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ApiConst } from '../../shared/constants';
 
 @Injectable()
 export class EmployeeProfileService  {
     constructor(private http: HttpClient) { }
     
     getEmployeeDetails(id) {
-        return this.http.get('api/employees/'+id);
+        return this.http.get(ApiConst.BASE_URL + 'employees/' + id);
     }
 }
