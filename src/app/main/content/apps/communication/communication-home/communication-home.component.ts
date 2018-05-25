3453e23a6d5b45e01d7c223e469aaf9989b43616
Yuvraj <ygaawde.edu@gmail.com>
2018-05-25 15:34:29 +0530

    Added 91 in the phone number

--- src/app/main/content/apps/communication/communication-home/communication-home.component.ts
+++ src/app/main/content/apps/communication/communication-home/communication-home.component.ts
@@ -169,7 +169,7 @@ export class CommunicationHomeComponent implements OnInit {
     if (this.selectedRecipient === 'employees') { 
       mobileNos = this.getEmployeeMobileNumbers();
       // obj['phonenumber'] = this.getEmployeeMobileNumbers();
-      msgObj['phonenumber'] = '7620676545';
+      msgObj['phonenumber'] = '917620676545';
     } else {
       // for stundent's parents 
     }
