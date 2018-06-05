375550956c81e77302241f593afb2ae30d8c0752
pavan.pawar <pavan.pawar@veritas.com>
2018-06-05 18:42:26 +0530

    changed prod setting

--- src/app/main/content/apps/shared/constants.ts
+++ src/app/main/content/apps/shared/constants.ts
@@ -1,5 +1,5 @@
 export class ApiConst {
-    public static readonly BASE_URL = 'http://dev.cloudscripts.co.in:8081/schoolManagement/';
+    public static readonly BASE_URL = 'http://demo.cloudscripts.co.in/schoolManagementProd/';
     public static readonly STUDENTS = '/students';
     // public static readonly BASE_URL = 'http://localhost:3000/';
 }
