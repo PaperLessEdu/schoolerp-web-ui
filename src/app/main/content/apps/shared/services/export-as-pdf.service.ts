5338455bc4b43058c06c1a542d68877c80f67bad
Yuvraj <ygaawde.edu@gmail.com>
2018-07-08 20:29:32 +0530

    Changes in the common jsPDF generateGrid method

--- src/app/main/content/apps/shared/services/export-as-pdf.service.ts
+++ src/app/main/content/apps/shared/services/export-as-pdf.service.ts
@@ -9,10 +9,10 @@ export class ExportAsPdfService {
     exportGridData(columns, gridData, fileName, pageTitle) {
         // Only pt supported (not mm or in)
         const doc = new jsPDF('p', 'pt');
-        doc.text(35, 25, pageTitle);
+        doc.text(40, 25, pageTitle);
         doc.autoTable(columns, gridData, {
             // Styling
-            theme: 'grid', // 'striped', 'grid' or 'plain'
+            theme: 'striped', // 'striped', 'grid' or 'plain'
             styles: {},
             headerStyles: {},
             bodyStyles: {},
